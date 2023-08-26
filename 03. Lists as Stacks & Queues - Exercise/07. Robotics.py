400e3bdb9ea00338f73dc85bba778773ac85433a
VankataLv <132002763+VankataLv@users.noreply.github.com>
2023-08-26 12:18:45 +0300

    26 AUG 23

--- 03. Lists as Stacks & Queues - Exercise/07. Robotics.py
+++ 03. Lists as Stacks & Queues - Exercise/07. Robotics.py
@@ -1,51 +1,30 @@
 from collections import deque
 from datetime import datetime, timedelta
 
+robot_date_initial = input().split(';')
+robot_db = deque()
+products_line = deque()
+start_time = datetime.strptime(input(), '%H:%M:%S')
+start_time = start_time.time()
 
-class Robot:
-    def __init__(self, name, process_time):
-        self.name = name
-        self.work_cap = int(process_time)
-        self.busy_until = None
+for el in robot_date_initial:
+    name, process_time = el.split("-")
+    robot = {'name': name, 'process_time': int(process_time), 'busy_until': start_time}
+    robot_db.append(robot)
 
-    def __str__(self):
-        return f"{self.name}-{self.work_cap}busy until:{self.busy_until}"
-
-    def is_free(self, current_time):
-        if self.busy_until is None or current_time >= self.busy_until:
-            return True
-        else:
-            return False
-
-    def start_processing(self, product, current_time):
-        self.busy_until = current_time + timedelta(seconds=self.work_cap)
-        print(f"{self.name} - {product} [{current_time.strftime('%H:%M:%S')}]")
-
-
-free_robots = deque()
-robot_data = input().split(";")
-for robot in robot_data:
-    name, process_time = robot.split("-")
-    process_time = int(process_time)
-    free_robots.append(Robot(name, process_time))
-
-
-start_time = datetime.strptime(input(), "%H:%M:%S")
-product_line = deque()
+cur_time = datetime(start_time)
 cmd = input()
-current_time = start_time
+while cmd != "End":
+    cur_time = cur_time + timedelta(seconds=1)
+    products_line.append(cmd)
+    while products_line:
+        for robot in robot_db:
+            if cur_time > robot["busy_until"]:
 
-while True:
-    current_time += timedelta(seconds=1)
-    product_line.append(cmd)
-    for robot in free_robots:
-        if not robot.busy_until:
-            robot.busy_until = True
-            print(f"{robot.name} - {product_line.popleft()} [{current_time.strftime('%H:%M:%S')}]")
-            for robot in free_robots:
-                print(robot)
-            break
+                print(f"{robot['name']} - {products_line.popleft()} [{cur_time}]")
+            else:
+                continue
+
     cmd = input()
 
-
-
+print("END____________")
