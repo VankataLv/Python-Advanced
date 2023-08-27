a7c4b6b3198f16005beb24766dc39b354647a31e
VankataLv <132002763+VankataLv@users.noreply.github.com>
2023-08-27 09:15:08 +0300

    26 AUG 23

--- 03. Lists as Stacks & Queues - Exercise/07. Robotics.py
+++ 03. Lists as Stacks & Queues - Exercise/07. Robotics.py
@@ -2,29 +2,38 @@ from collections import deque
 from datetime import datetime, timedelta
 
 robot_date_initial = input().split(';')
-robot_db = deque()
+robot_db = {}
 products_line = deque()
 start_time = datetime.strptime(input(), '%H:%M:%S')
-start_time = start_time.time()
 
 for el in robot_date_initial:
     name, process_time = el.split("-")
-    robot = {'name': name, 'process_time': int(process_time), 'busy_until': start_time}
-    robot_db.append(robot)
+    robot_db[name] = {'process_time': int(process_time), 'busy_until': start_time}
 
-cur_time = datetime(start_time)
+cur_time = start_time
+
 cmd = input()
-while cmd != "End":
+while True:
     cur_time = cur_time + timedelta(seconds=1)
     products_line.append(cmd)
+    # print(datetime.strftime(cur_time, '%H:%M:%S'))  #  for debugging purposes
     while products_line:
-        for robot in robot_db:
-            if cur_time > robot["busy_until"]:
+        for key, value in robot_db.items():
+            if cur_time > robot_db[key]["busy_until"]:
+                print(f"{key} - {products_line.popleft()} [{datetime.strftime(cur_time, '%H:%M:%S')}]")
+                robot_db[key]["busy_until"] = cur_time + timedelta(seconds=robot_db[key]['process_time'] - 1)
+                if not products_line:
+                    break
+        else:
+            break
+    if cmd != "End":
+        cmd = input()
+    if products_line:
+        if products_line[0] == "End":
+            break
 
-                print(f"{robot['name']} - {products_line.popleft()} [{cur_time}]")
-            else:
-                continue
 
-    cmd = input()
-
-print("END____________")
+# print("END____________")   #  for debugging purposes
+# print(cur_time)
+# print(products_line)
+# print(robot_db)
