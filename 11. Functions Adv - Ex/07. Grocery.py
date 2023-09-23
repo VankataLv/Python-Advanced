54ab5e11ac63eb58e8ab470cc2b5d160b3730de4
VankataLv <132002763+VankataLv@users.noreply.github.com>
2023-09-23 10:43:03 +0300

    23 SEP 23

--- 11. Functions Adv - Ex/07. Grocery.py
+++ 11. Functions Adv - Ex/07. Grocery.py
@@ -1,11 +1,19 @@
 def grocery_store(**kwargs):
+    working_dict = sorted(kwargs.items(), key=lambda x: (-x[1], -len(x[0]), x[0] ))
+    result = []
+    for el in working_dict:
+        result.append(f"{el[0]}: {el[1]}")
+    return "\n".join([str(el) for el in result])
 
-    result = dict(sorted(kwargs.items(), key=lambda x: kwargs[x]))
-    print(result)
 
-
+# print(grocery_store(
+#     bread=5,
+#     pasta=12,
+#     eggs=12,
+# ))
 print(grocery_store(
-    bread=5,
-    pasta=12,
-    eggs=12,
+    bread=2,
+    pasta=2,
+    eggs=20,
+    carrot=1,
 ))
